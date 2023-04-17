4a9e0841f6980064dfd8e26a93b346e8b9e94850
Imanrostami <imanrostami8219@gmail.com>
2023-04-17 17:07:44 +0430

    add total file

---
+++ apprevers/Controllers/App revers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App_Revers.Controllers;
+[ApiController]
+[Route("api/[controller],[action]")]
+public class App_revers:ControllerBase
+{
+   [HttpGet]
+   public string Reverse(string name )
+   {
+   
+       char[] charArray = name.ToCharArray();
+       Array.Reverse(charArray);
+       return new string(charArray);
+           
+   }
+}
---
+++ asp.net core projects/Controllers/ssss.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebApplication6.Controllers;
+
+[ApiController]
+[Route("api/[controller],[action]")]
+
+public class ReversController:ControllerBase
+{
+    
+    [HttpGet]
+    public string Reverse(string name )
+    {
+
+        char[] charArray = name.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+        
+    }
+    
+  
+
+
+}
